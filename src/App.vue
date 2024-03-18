175cb428bb853415ec1720812079192f7b97996f
Juanan <juanantonioamillopez@gmail.com>
2024-03-18 11:34:50 +0100

    SFC (Single File Component)

--- src/App.vue
+++ src/App.vue
@@ -1,3 +1,22 @@
+<!-- 
+  Aquí colocaremos la lógica de nuestras aplicaciones de VUE
+ -->
+<script setup>
+console.log('Hola Vue');
+</script>
+
+<!-- 
+  Aquí colocaremos TODAS las etiquetas HTML/JSX para la estructura de nuestras aplicaciones de VUE
+ -->
 <template>
   <h1>Hola Vue</h1>
 </template>
+
+<!-- 
+  Aquí renderizaremos los estilos
+ -->
+<style>
+h1 {
+  color: red;
+}
+</style>
