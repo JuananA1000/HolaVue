3ac0a14afaa5e7ae411f2da41ec1abac50246ba2
Juanan <juanantonioamillopez@gmail.com>
2024-03-18 12:03:46 +0100

    V-Bind. La comunicación entre las etiquetas SCRIPT y TEMPLATE

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,15 @@
 <script setup>
 const nombre = 'Juanan';
+const colorDeEstilo = 'color: blue';
+
 console.log('Hola Vue');
 </script>
 
 <template>
   <h1>Hola Vue</h1>
   <h1>Hola {{ nombre || 'valor por defecto' }}</h1>
+  <h1 v-bind:style="colorDeEstilo">Hola Azul</h1>
+  <h1 :style="colorDeEstilo">Hola Azul también</h1>
 </template>
 
 <style>
